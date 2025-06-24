b636312e895f96f2e4e563a32bbec68799c5bb8e
Oliver Pajonk <oliver.pajonk@elektrobit.com>
2025-06-24 15:27:08 +0000

    Fix install script to use apt-get for package management consistency

--- src/s-core-devcontainer/.devcontainer/local/install.sh
+++ src/s-core-devcontainer/.devcontainer/local/install.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 # GraphViz
-apt update
-apt install -y graphviz
+apt-get update
+apt-get install -y graphviz
 
 # Bazel, via APT
 # - ghcr.io/devcontainers-community/features/bazel uses bazelisk, which has a few problems:
@@ -12,12 +12,12 @@ apt install -y graphviz
 #   - In general, pre-built containers *shall not* download "more tools" from the internet.
 #     This is an operational risk (security, availability); it makes the build non-reproducible,
 #     and it prevents the container from working in air-gapped environments.
-apt install apt-transport-https curl gnupg -y
+apt-get install apt-transport-https curl gnupg -y
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel-archive-keyring.gpg
 mv bazel-archive-keyring.gpg /usr/share/keyrings
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-apt update
-apt install -y bazel=${BAZEL_VERSION}
+apt-get update
+apt-get install -y bazel=${BAZEL_VERSION}
 
 # Cleanup
 apt-get autoremove -y
