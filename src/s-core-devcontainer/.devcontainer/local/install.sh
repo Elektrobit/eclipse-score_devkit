2a784c8d8967ec8c234d3d5305390c382eb95a78
Oliver Pajonk <oliver.pajonk@elektrobit.com>
2025-06-25 09:02:28 +0000

    Add Bazel Compile Commands installation to devcontainer setup

--- src/s-core-devcontainer/.devcontainer/local/install.sh
+++ src/s-core-devcontainer/.devcontainer/local/install.sh
@@ -24,6 +24,7 @@ apt-get update
 apt-get install -y bazel=${BAZEL_VERSION}
 
 # Code completion for C++ code of bazel projects
+# see https://github.com/kiron1/bazel-compile-commands
 source /etc/lsb-release
 curl https://github.com/kiron1/bazel-compile-commands/releases/download/v${BAZEL_COMPILE_COMMANDS_VERSION}/bazel-compile-commands_${BAZEL_COMPILE_COMMANDS_VERSION}-${DISTRIB_CODENAME}_amd64.deb -o bazel-compile-commands.deb
 dpkg -i  bazel-compile-commands.deb
