0129bdc7e0afa316648706661dd81eb8187e60fb
Oliver Pajonk <oliver.pajonk@elektrobit.com>
2025-06-25 15:54:02 +0000

    Update build script: enable image push during devcontainer build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 IMAGE="$1"
 
-devcontainer build --workspace-folder src/${IMAGE} --push false --image-name ghcr.io/elektrobit/eclipse-score_devkit:latest
+devcontainer build --workspace-folder src/${IMAGE} --push true --image-name ghcr.io/elektrobit/eclipse-score_devkit:latest
