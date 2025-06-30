a61f6c7af69bbb9670e05cbaa5c3d4d0cffbcf4b
Oliver Pajonk <oliver.pajonk@elektrobit.com>
2025-06-30 06:48:50 +0000

    do not check for versions
    this is probably "overkill" here

--- src/s-core-devcontainer/.devcontainer/s-core-local/tests/test_default.sh
+++ src/s-core-devcontainer/.devcontainer/s-core-local/tests/test_default.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-check "validate bazel is working and has the correct version" bash -c "bazel version | grep 'Build label: 7.5.0'"
-check "validate bazel-compile-commands is working and has the correct version" bash -c "bazel-compile-commands --version 2>&1| grep 'bazel-compile-commands: 0.17.2'"
-check "validate buildifier is working and has the correct version" bash -c "buildifier --version | grep 'buildifier version: 8.2.1'"
+check "validate bazel is working and has the correct version" bash -c "bazel version"
+check "validate bazel-compile-commands is working and has the correct version" bash -c "bazel-compile-commands --version"
+check "validate buildifier is working and has the correct version" bash -c "buildifier --version"
 check "validate git is working" bash -c "git --version"
