fd53a06299f301ef89d63ecf459aaa07cccee946
Oliver Pajonk <oliver.pajonk@elektrobit.com>
2025-07-01 11:44:43 +0000

    fix: update build and publish scripts to use dynamic image versioning

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,5 +2,6 @@
 set -euo pipefail
 
 IMAGE="$1"
+VERSION="${2:-latest}"
 
-devcontainer build --workspace-folder src/${IMAGE} --push true --image-name ghcr.io/elektrobit/eclipse-score_devkit:latest
+devcontainer build --workspace-folder src/${IMAGE} --push true --image-name ghcr.io/elektrobit/eclipse-score_devkit:${VERSION}
