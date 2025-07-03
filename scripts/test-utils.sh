8120c0e961b99213921afd6c0a57cd4085ff54a3
Oliver Pajonk <oliver.pajonk@elektrobit.com>
2025-07-03 10:58:25 +0000

    refactor: remove unused test functions from test-utils.sh

--- scripts/test-utils.sh
+++ scripts/test-utils.sh
@@ -26,58 +26,6 @@ check() {
     fi
 }
 
-checkVersionGe() {
-    LABEL=$1
-    CURRENT_VERSION=$2
-    REQUIRED_VERSION=$3
-    shift
-    echo -e "\n🧪 Testing ${LABEL}: '${CURRENT_VERSION}' is >= '${REQUIRED_VERSION}'"
-    local GREATER_VERSION
-    GREATER_VERSION=$( (echo "${CURRENT_VERSION}"; echo "${REQUIRED_VERSION}") | sort -V | tail -1)
-    if [[ "${CURRENT_VERSION}" == "${GREATER_VERSION}" ]]; then
-        echo "✅  Passed!"
-        return 0
-    else
-        echoStderr "❌ ${LABEL} check failed."
-        FAILED+=("${LABEL}")
-        return 1
-    fi
-}
-
-checkMultiple() {
-    PASSED=0
-    LABEL="$1"
-    echo -e "\n🧪 Testing ${LABEL}."
-    shift; MINIMUMPASSED=$1
-    shift; EXPRESSION="$1"
-    while [[ "${EXPRESSION}" != "" ]]; do
-        if ${EXPRESSION}; then ((PASSED++)); fi
-        shift; EXPRESSION=$1
-    done
-    if [[ ${PASSED} -ge ${MINIMUMPASSED} ]]; then
-        echo "✅ Passed!"
-        return 0
-    else
-        echoStderr "❌ ${LABEL} check failed."
-        FAILED+=("${LABEL}")
-        return 1
-    fi
-}
-
-checkOSPackages() {
-    LABEL=$1
-    shift
-    echo -e "\n🧪 Testing ${LABEL}"
-    if dpkg-query --show -f='${Package}: ${Version}\n' "$@"; then
-        echo "✅  Passed!"
-        return 0
-    else
-        echoStderr "❌ ${LABEL} check failed."
-        FAILED+=("${LABEL}")
-        return 1
-    fi
-}
-
 reportResults() {
     if [[ ${#FAILED[@]} -ne 0 ]]; then
         echoStderr -e "\n💥  Failed tests:" "${FAILED[@]}"
